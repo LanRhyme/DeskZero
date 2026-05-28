3385c339e99890f86deea51ba0c78d17f517a866
LanRhyme <xiao_ren233@foxmail.com>
2026-05-28 16:46:48 +0800

    feat: initialize theme listener in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useEffect } from 'react'
 import DesktopLayer from '@/components/Desktop/DesktopLayer'
 import { SettingsPage } from '@/components/Settings/SettingsPage'
+import { useSettingsStore } from '@/stores/settingsStore'
 
 function App() {
   const isSettings = window.location.pathname === '/settings'
+  const { loadSettings, initThemeListener } = useSettingsStore()
+
+  useEffect(() => {
+    loadSettings()
+    const cleanup = initThemeListener()
+    return cleanup
+  }, [loadSettings, initThemeListener])
 
   if (isSettings) {
     return <SettingsPage />
